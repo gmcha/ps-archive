ceaebacc9e3f38856155b14db1764c204c926e23
Ga Min Cha <prettygamin@naver.com>
2026-01-04 11:13:16 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -7,15 +7,14 @@ int main(){
 
     int n;
     cin >> n;
-
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i - 1; j++){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++){
             cout << " ";
         }
-        for(int j = 1; j <= n - i + 1; j++){
+        for(int k = n - i; k > 0; k-- ){
             cout << "*";
         }
-        for(int j = 1; j <= n - i; j++){
+        for(int k = n - i; k > 1; k-- ){
             cout << "*";
         }
         cout << "\n";
