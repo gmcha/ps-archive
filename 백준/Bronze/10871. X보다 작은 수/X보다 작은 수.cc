e29d438c6d88073f6a5f3411b9d3d75efea67af5
Ga Min Cha <prettygamin@naver.com>
2026-01-04 12:35:04 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,13 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int N, X;
-    cin >> N >> X;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    int item = 0;
-    for(int i = 0; i < N; i++){
-        cin >> item;
-        if(item < X) cout << item << ' ';
+    int n, x;
+    cin >> n >> x;
+
+    int a;
+    for(int i = 0; i < n; i++){
+        cin >> a;
+        if(a < x) cout << a << " ";
     }
 }
