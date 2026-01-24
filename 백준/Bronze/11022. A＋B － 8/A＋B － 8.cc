de305c8b829692992e90f2c3bbe39ff149d89736
Ga Min Cha <prettygamin@naver.com>
2026-01-24 12:56:22 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    int A, B;
+    for(int i = 0; i < T; i++){
+        cin >> A >> B;
+        cout << "Case #" << i + 1 << ": " << A << " + " << B << " = " << A + B << "\n";
+    }
+}
