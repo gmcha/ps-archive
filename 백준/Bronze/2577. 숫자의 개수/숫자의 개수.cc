84af7fdf57582ac1aaeb997f5d4ec914f1d9192e
Ga Min Cha <prettygamin@naver.com>
2026-03-19 15:20:16 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -5,19 +5,30 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int nums[10] = {};
-    int a, b, c;
-    cin >> a >> b >> c;
+    // 배열(10개 칸) - 초기값 0
+    // 입력: A, B, C
+    // product 찾기
+    
+    // (product의 각 자릿수를 순회하며) - 10번 이하
+    // [인덱스: 그 자리에 수]에 ++하기
 
-    int product = a * b * c;
+    // 출력: 각 배열 칸의 값 반환
+
+    int arr[10];
+    fill(arr, arr + 10, 0);
+
+    int A, B, C;
+    cin >> A >> B >> C;
+    int product = A*B*C;
+
     while(product > 0) {
-        nums[product % 10]++;
+        int digit = product % 10;
+        arr[digit]++;
         product /= 10;
     }
 
     for(int i = 0; i < 10; i++){
-        cout << nums[i] << "\n";
+        cout << arr[i] << "\n";
     }
-
 
 }
