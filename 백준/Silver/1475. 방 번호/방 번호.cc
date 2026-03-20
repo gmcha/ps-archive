913d1568a32002cf10deb50b480b3f5372928389
Ga Min Cha <prettygamin@naver.com>
2026-03-20 15:03:34 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
- 
+
     // 재료: 배열[10], 각 원소 0
     int arr[10] = {};
     
@@ -21,7 +21,7 @@ int main(){
 
     // arr[6] + arr[9] = sum, sum이 홀수면 ++하고 /2, 짝수면 그냥 /2
     int sum = arr[6] + arr[9];
-    if(sum % 2 == 1) sum += 1;
+    if(sum % 2 != 0) sum += 1;
     sum /= 2;
 
     // 그 값(변형된 sum)을 각 arr[6], arr[9]에 넣음
