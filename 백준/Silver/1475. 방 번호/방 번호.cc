fa739f65d9d09d36b984fe3b180b06f791f730d7
Ga Min Cha <prettygamin@naver.com>
2026-03-20 09:09:57 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -4,40 +4,36 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-
-    // 입력: N
+ 
+    // 재료: 배열[10], 각 원소 0
+    int arr[10] = {};
+    
+    // 입력: 방 번호 N
     int N;
     cin >> N;
 
-    // 방 번호의 각 자릿수를 배열에 넣음
-    int arr[10] = {};
-
+    // N의 digits마다, 배열[digit]++하기
     while(N > 0) {
         int digit = N % 10;
         arr[digit]++;
         N /= 10;
     }
-    
-    // 6과 9의 처리 이후, index6/9는 처리 수로 채워넣기
-    int sum = arr[6] + arr[9];
-    if(sum % 2 == 1) {
-        sum += 1;
-    }
-    sum /= 2; // 6, 9 처리 수
 
+    // arr[6] + arr[9] = sum, sum이 홀수면 ++하고 /2, 짝수면 그냥 /2
+    int sum = arr[6] + arr[9];
+    if(sum % 2 == 1) sum += 1;
+    sum /= 2;
+
+    // 그 값(변형된 sum)을 각 arr[6], arr[9]에 넣음
     arr[6] = sum;
     arr[9] = sum;
-    
-    // 배열의 수 중 가장 큰 수를 찾기
-    int max_num = 0;
+
+    // 배열의 원소 중, 최댓값 구하기
+    int max_num = 0; 
     for(int i = 0; i < 10; i++){
         if(arr[i] > max_num) max_num = arr[i];
     }
 
-    // (그 수 반환)
+    // 출력: 최댓값
     cout << max_num;
-
-
-
 }
