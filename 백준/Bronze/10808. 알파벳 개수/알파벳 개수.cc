a5227b42831f93865c6fa3d2f161f4ab72945c69
Ga Min Cha <prettygamin@naver.com>
2026-03-23 16:41:04 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -5,23 +5,21 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // 배열 26짜리, 0 넣음. (fill)
-    // S에서 alphabet 1나씩 받으면서
-    // 인덱스가 (alphabet - 'a')인 곳에 ++하기. 
-    // 출력: 배열의 각 원소
+    // 배열 - 26개 (각 알파벳의 수 들어 있음)
+    // 초반 배열 -> 모든 원소 0
+    int arr[26] = {};
 
-    int arr[26];
-    fill(arr, arr + 26, 0);
-
+    // 입력: 알파벳 소문자로만 이루어진 단어 S
     string S;
     cin >> S;
 
-    for(char c : S){
+    // 각 알파벳에 대하여, arr˜[알파벳-'a']++
+    for(char c : S) {
         arr[c - 'a']++;
     }
 
+    // 출력: 배열의 모든 원소
     for(int i = 0; i < 26; i++){
         cout << arr[i] << " ";
     }
-
 }
