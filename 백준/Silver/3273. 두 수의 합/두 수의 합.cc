d481049ddf8d618008d5af2977a01e9484b0249a
Ga Min Cha <prettygamin@naver.com>
2026-03-28 15:45:15 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 10224 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,36 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[10000002];
-int numbers[10000002];
+int n, x;
+int arr[100002];
+int occur[2000002];
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // n개의 int (a)
-    // x: int (sum)
-    // 출력: 쌍의 수
-    // O(n), 1.2억 / 4  -> 3000만
-
-    int n;
     cin >> n;
-
-    for(int i = 0; i < n; i++){
-        cin >> arr[i];
-        numbers[arr[i]] = 1;
-    }
-
-    int x;
+    for(int i = 0; i < n; i++) cin >> arr[i];
     cin >> x;
 
     int ans = 0;
     for(int i = 0; i < n; i++) {
-        if(arr[i] >= x) continue;
-        if(numbers[x - arr[i]]) ans++; 
+        if(x >= arr[i] && occur[x-arr[i]]){ // 존재 한다면
+            ans++;
+        }
+        occur[arr[i]]++;
     }
 
-    cout << ans / 2;
 
-
+    cout << ans;
 }
