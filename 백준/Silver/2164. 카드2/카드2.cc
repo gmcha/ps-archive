f47cdb0549910c74a5d299a657a5a62147b037b7
Ga Min Cha <prettygamin@naver.com>
2026-03-28 15:54:35 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    // 초기 queue 채우기
+    queue<int> Q;
+    for(int i = 1; i <= N; i++) Q.push(i);
+
+    while(Q.size() != 1) {
+        Q.pop();
+        if(Q.size() == 1) break;
+        Q.push(Q.front());
+        Q.pop();
+    }
+
+    cout << Q.front();
+}
