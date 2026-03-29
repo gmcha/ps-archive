8af43667da513b95952b8586af8753e00514231e
Ga Min Cha <prettygamin@naver.com>
2026-03-29 18:14:27 +0900

    [Silver I] Title: 그림, Time: 16 ms, Memory: 3988 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -5,6 +5,7 @@ using namespace std;
 
 int board[502][502];
 int vis[502][502];
+int n, m;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
@@ -12,44 +13,43 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m;
     cin >> n >> m;
-
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             cin >> board[i][j];
         }
     }
 
-    // 그림의 개수, 가장 넓은 넓이
+    queue<pair<int, int>> Q;
+
     int num_pics = 0, max_area = 0;
 
-    // 각 그림 뭉치의 시작점에 대하여
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             if(vis[i][j] || board[i][j] != 1) continue;
-
-            queue<pair<int, int>> Q;
+            num_pics++;
             vis[i][j] = 1;
             Q.push({i, j});
-            num_pics++;
 
             int cur_area = 0;
+            cur_area++;
             while(!Q.empty()){
-                pair<int, int> cur = Q.front(); Q.pop(); cur_area++;
+                pair<int, int> cur = Q.front(); Q.pop();
 
                 for(int dir = 0; dir < 4; dir++){
                     int nx = cur.X + dx[dir];
                     int ny = cur.Y + dy[dir];
                     if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
                     if(vis[nx][ny] || board[nx][ny] != 1) continue;
+                    cur_area++;
                     vis[nx][ny] = 1;
                     Q.push({nx, ny});
                 }
             }
             max_area = max(max_area, cur_area);
+            
         }
     }
-    
+
     cout << num_pics << "\n" << max_area;
 }
