5cd69e28b93f4af8335adf19d96340de6d4780bd
Ga Min Cha <prettygamin@naver.com>
2026-04-08 14:19:19 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2204 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -3,9 +3,9 @@ using namespace std;
 #define X first
 #define Y second
 
-string board[102];
-int dist[102][102];
 int n, m;
+string board[105];
+int dist[105][105];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
@@ -17,30 +17,25 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> board[i];
     }
-    
-    // dist를 -1로 초기화하기
-    for(int i = 0; i < n; i++){
-        fill(dist[i], dist[i] + 102, -1);
-    }
 
     queue<pair<int, int>> Q;
-    dist[0][0] = 0;
     Q.push({0, 0});
+    dist[0][0] = 0;
 
     while(!Q.empty()){
         pair<int, int> cur = Q.front(); Q.pop();
+        
         for(int dir = 0; dir < 4; dir++){
             int nx = cur.X + dx[dir];
             int ny = cur.Y + dy[dir];
 
             if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if(dist[nx][ny] != -1 || board[nx][ny] != '1') continue;
+            if(dist[nx][ny] != 0 || board[nx][ny] != '1') continue;
             dist[nx][ny] = dist[cur.X][cur.Y] + 1;
             Q.push({nx, ny});
         }
     }
 
-    cout << dist[n -1][m - 1] + 1;
-
+    cout << dist[n - 1][m - 1] + 1;
 
 }
