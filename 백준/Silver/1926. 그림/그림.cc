99cc7dfb3a3adca0174538c71e97b91ff8a2b35f
Ga Min Cha <prettygamin@naver.com>
2026-04-08 11:09:22 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3988 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -3,9 +3,9 @@ using namespace std;
 #define X first
 #define Y second
 
+int n, m;
 int board[502][502];
 int vis[502][502];
-int n, m;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
@@ -22,34 +22,35 @@ int main(){
 
     queue<pair<int, int>> Q;
 
-    int num_pics = 0, max_area = 0;
+    int num_pics = 0;
+    int max_area = 0;
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            if(vis[i][j] || board[i][j] != 1) continue;
-            num_pics++;
-            vis[i][j] = 1;
+            if(vis[i][j] || board[i][j] != 1) continue; 
             Q.push({i, j});
+            vis[i][j] = 1;
+            num_pics++;
 
-            int cur_area = 0;
-            cur_area++;
+            int area = 0;
             while(!Q.empty()){
                 pair<int, int> cur = Q.front(); Q.pop();
+                area++;
 
                 for(int dir = 0; dir < 4; dir++){
                     int nx = cur.X + dx[dir];
                     int ny = cur.Y + dy[dir];
                     if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                    if(vis[nx][ny] || board[nx][ny] != 1) continue;
-                    cur_area++;
-                    vis[nx][ny] = 1;
+                    if(vis[nx][ny] || board[nx][ny] != 1) continue; 
+
                     Q.push({nx, ny});
+                    vis[nx][ny] = 1;
                 }
             }
-            max_area = max(max_area, cur_area);
-            
+            max_area = max(max_area, area);
         }
     }
 
     cout << num_pics << "\n" << max_area;
+    
 }
