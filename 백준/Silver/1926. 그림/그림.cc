d8f71f1b4b6aa1a2146de4d8c7611bd0b3fcbce7
Ga Min Cha <prettygamin@naver.com>
2026-04-21 10:25:37 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3988 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -21,36 +21,35 @@ int main(){
     }
 
     queue<pair<int, int>> Q;
-
-    int num_pics = 0;
+    int num = 0;
     int max_area = 0;
-
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            if(vis[i][j] || board[i][j] != 1) continue; 
-            Q.push({i, j});
-            vis[i][j] = 1;
-            num_pics++;
+            if(vis[i][j] || board[i][j] != 1) continue;
 
             int area = 0;
+            num++;
+            area++;
+            vis[i][j] = 1;
+            Q.push({i, j});
+
             while(!Q.empty()){
                 pair<int, int> cur = Q.front(); Q.pop();
-                area++;
 
                 for(int dir = 0; dir < 4; dir++){
                     int nx = cur.X + dx[dir];
                     int ny = cur.Y + dy[dir];
                     if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                    if(vis[nx][ny] || board[nx][ny] != 1) continue; 
-
+                    if(vis[nx][ny] || board[nx][ny] != 1) continue;
                     Q.push({nx, ny});
                     vis[nx][ny] = 1;
+                    area++;
                 }
             }
+
             max_area = max(max_area, area);
         }
     }
 
-    cout << num_pics << "\n" << max_area;
-    
+    cout << num << "\n" << max_area;
 }
